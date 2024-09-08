f3aceac8934c05a3c8398e34737aee156803daa1
Liam Cottrell <lemcottrell@gmail.com>
2024-09-08 19:07:41 +0100

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { FaReact } from "react-icons/fa";
+
+const year = 2024;
 
 const Footer = () => {
   return (
     <div>
-        <p>Made by Liam Cottrell.</p>
-        
+        <p>Made by Liam Cottrell © {year}.</p>
+        <p><FaReact size={26}/></p>
+
     </div>
   )
 }
