d200b3510e45e44e2900ce61032e59583fdad9d6
Liam Cottrell <lemcottrell@gmail.com>
2024-09-14 21:24:34 +0100

    start about

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const About = () => {
+  return (
+    <div className='w-50 mx-auto mb-5'>
+            <p class="text-center" id="bio-text">
+                  Welcome to my portfolio! I'm an aspiring Data Scientist from the UK, doing whatever I can to gain the skills I need in data and coding to make this my career. 
+                  I'm a graduate of edX's Front-End Web Development bootcamp, which taught me fundamental skills including HTML, CSS, JavaScript and React.
+                  And I just completed a 12-week front-end internship at <a href="https://socialwebstrategy.com/" target="_blank">Social Web Strategy</a>. <br />
+                  Feel free to take a look at my recent projects below, and don't hesitate to get in touch! 
+                </p>
+    </div>
+  )
+}
+
+export default About
--- src/pages/AppBody.jsx
+++ src/pages/AppBody.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Contact from '../components/Contact'
 import Certs from '../pages/Certs'
+import About from './About'
 
 const AppBody = () => {
   return (
     <div className='app-body'>
         <Contact />
         {/* <Certs /> */}
+        <About />
     </div>
   )
 }
