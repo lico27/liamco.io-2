6a0bd1f44f8aaae2ff0c598ca3816fcead588194
Liam Cottrell <lemcottrell@gmail.com>
2024-09-18 12:23:16 +0100

    make routes

--- src/pages/AppBody.jsx
+++ src/pages/AppBody.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GreetingTemp from '../components/GreetingTemp'
-import Certs from '../pages/Certs'
 import About from './About'
+import Certs from '../pages/Certs'
 import Projects from './Projects'
+import Blog from './Blog'
+import Contact from './Contact'
 
 const AppBody = () => {
   return (
     <div className='app-body mb-5'>
-        <GreetingTemp />
-        {/* <Certs /> */}
-        {/* <About /> */}
-        {/* <Projects /> */}
+        <Router>
+          <Routes>
+            <Route path='/' element={<GreetingTemp />} />
+            {/* <Route path='/about' element={<About />} /> */}
+            <Route path='/certs' element={<Certs />} />            
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/blog' element={<Blog />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Router>
     </div>
   )
 }
