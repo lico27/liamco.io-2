c292934248100bca88f29b63fa9134327943e592
Liam Cottrell <lemcottrell@gmail.com>
2024-10-08 15:33:29 +0100

    start contact

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-50 mx-auto mb-5'>
+<>
+    <div className='w-100 flex text-center py-5'>
       <h1 className='mb-4 text-capitalize text-general'>Hi, I'm Liam</h1>
-      <img src="src/assets/img/pfp1.png" id="pfp" className='mx-auto'/>
-            <p class="text-center" id="bio-text">
-                  Welcome to my portfolio! I'm an aspiring Data Scientist from the UK, doing whatever I can to gain the skills I need in data and coding to make this my career. 
-                  I'm a graduate of edX's Front-End Web Development bootcamp, which taught me fundamental skills including HTML, CSS, JavaScript and React.
-                  And I just completed a 12-week front-end internship at <a href="https://socialwebstrategy.com/" target="_blank">Social Web Strategy</a>. <br />
-                  Feel free to take a look at my recent projects below, and don't hesitate to get in touch! 
-                </p>
-    </div>
+        <img src="src/assets/img/pfp1.png" id="pfp" className='mb-4 '/>
+              <p className='mx-5 text-general'>
+                    Welcome to my portfolio! I'm an aspiring Data Scientist from the UK, doing whatever I can to gain the skills I need in data and coding to make this my career. 
+                    I'm a graduate of edX's Front-End Web Development bootcamp, which taught me fundamental skills including HTML, CSS, JavaScript and React.
+                    And I just completed a 12-week front-end internship at <a href="https://socialwebstrategy.com/" target="_blank">Social Web Strategy</a>. <br />
+                    Feel free to take a look at my recent projects below, and don't hesitate to get in touch! 
+                  </p>
+      </div>
+</>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { 
+  FaGithub, 
+  FaEnvelope, 
+  FaLinkedin 
+} from 'react-icons/fa';
+import { FaThreads } from 'react-icons/fa6';
 
 const Contact = () => {
   return (
     <div className='w-100 flex text-center py-5'>
-      <h1 className='m-0 text-capitalize'>
-        Coming soon
-      </h1> 
+        <h1 className='mb-4 text-capitalize text-general'>Contact</h1>
+        <p className='mb-1 text-general'>My portfolio is a work in progress.</p>
+        <p className='mb-2 text-general'>You can contact me here:</p>
+        <p>
+          <a href='https://github.com/lico27' target='_blank'><FaGithub className='contact-icons m-1'/></a>
+          <a href='mailto:liam@liamco.io' target='_blank'><FaEnvelope className='contact-icons m-1'/></a>
+          <a href='https://www.linkedin.com/in/liam-cottrell-a1837272/' target='_blank'><FaLinkedin className='contact-icons m-1'/></a>
+          <a href='https://www.threads.net/@__lico27' target='_blank'><FaThreads className='contact-icons m-1'/></a>
+        </p>
+        
     </div>
   )
 }
