f4c9d0366af7d9207098896e2ded75cbc24fa9b4
Liam Cottrell <lemcottrell@gmail.com>
2024-10-08 15:57:19 +0100

    build about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 const About = () => {
   return (
 <>
-    <div className='w-100 flex text-center py-5'>
+    <div className='w-100 flex text-center pt-5'>
       <h1 className='mb-4 text-capitalize text-general'>Hi, I'm Liam</h1>
+      <div className='about-container mx-auto col-10 col-md-6 col-lg-4'>
+      <Card 
+      bg='dark' 
+      border='light'
+      text='light' 
+      className='border-0'
+    >      
+      <Card.Body>
+        <Card.Title className='mt-1 w-100'>
         <img src="src/assets/img/pfp1.png" id="pfp" className='mb-4 '/>
-              <p className='mx-5 text-general'>
-                    Welcome to my portfolio! I'm an aspiring Data Scientist from the UK, doing whatever I can to gain the skills I need in data and coding to make this my career. 
+        </Card.Title>
+        <Card.Text>
+        <p className='text-general'>
+                    Welcome to my portfolio! I'm an aspiring Data Scientist from the UK. 
                     I'm a graduate of edX's Front-End Web Development bootcamp, which taught me fundamental skills including HTML, CSS, JavaScript and React.
                     And I just completed a 12-week front-end internship at <a href="https://socialwebstrategy.com/" target="_blank">Social Web Strategy</a>. <br />
                     Feel free to take a look at my recent projects below, and don't hesitate to get in touch! 
                   </p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        
+              
+      </div>
       </div>
 </>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,10 +13,10 @@ const Contact = () => {
         <p className='mb-1 text-general'>My portfolio is a work in progress.</p>
         <p className='mb-2 text-general'>You can contact me here:</p>
         <p>
-          <a href='https://github.com/lico27' target='_blank'><FaGithub className='contact-icons m-1'/></a>
-          <a href='mailto:liam@liamco.io' target='_blank'><FaEnvelope className='contact-icons m-1'/></a>
-          <a href='https://www.linkedin.com/in/liam-cottrell-a1837272/' target='_blank'><FaLinkedin className='contact-icons m-1'/></a>
-          <a href='https://www.threads.net/@__lico27' target='_blank'><FaThreads className='contact-icons m-1'/></a>
+          <a href='https://github.com/lico27' target='_blank'><FaGithub className='contact-icons m-3'/></a>
+          <a href='mailto:liam@liamco.io' target='_blank'><FaEnvelope className='contact-icons m-3'/></a>
+          <a href='https://www.linkedin.com/in/liam-cottrell-a1837272/' target='_blank'><FaLinkedin className='contact-icons m-3'/></a>
+          <a href='https://www.threads.net/@__lico27' target='_blank'><FaThreads className='contact-icons m-3'/></a>
         </p>
         
     </div>
