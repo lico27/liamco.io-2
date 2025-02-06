a60411efdb0373ed31ce87b573bd05697883d9d4
Liam Cottrell <lemcottrell@gmail.com>
2025-02-06 17:27:16 +0000

    edit project cards

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -12,29 +12,11 @@ import {
 } from "react-icons/fa";
 import {
     SiJavascript,
+    SiTidyverse,
+    SiKaggle
 } from "react-icons/si";
 
 export const projects = [
-    {
-        title: 'travel.ler',
-        description: 'A dynamic travel dashboard for visitors to the UK, providing relevant data like weather, currency and news',
-        url: 'https://lico27.github.io/travelLER/',
-        status: 'Complete',
-        img: travellerImage,
-        subtitle: 'Bootcamp Capstone Project #1',
-        icons: [FaCss3Alt, FaHtml5, SiJavascript, FaBootstrap],
-        type: 'Web Development',
-    },
-    {
-        title: 'Pawesome Pets',
-        description: 'A resource to help prospective cat and dog owners choose a breed, learn facts, and pick a name for their furry friend',
-        url: 'https://main--sunny-semifreddo-f70c5b.netlify.app/#',
-        status: 'Complete',
-        img: pawesomeImage,
-        subtitle: 'Bootcamp Capstone Project #2',
-        icons: [FaCss3Alt, FaHtml5, SiJavascript, FaReact, FaBootstrap, FaNodeJs, FaNpm],
-        type: 'Web Development',
-    },
     {
         title: 'Worst. Analysis. Ever.',
         description: 'An exploration of Simpsons episodes in R',
@@ -42,9 +24,29 @@ export const projects = [
         url: 'TBC',
         status: 'In progress',
         img: simpsonsImage,
-        subtitle: 'TidyTuesdays Project #1',
-        icons: [FaRProject],
+        subtitle: 'TidyTuesdays Project #1 - Feb 2025',
+        icons: [FaRProject, SiTidyverse, SiKaggle],
         type: 'Data Analysis',
+    },
+    {
+        title: 'Pawesome Pets',
+        description: 'A resource to help prospective cat and dog owners choose a breed, learn facts, and pick a name for their furry friend',
+        url: 'https://main--sunny-semifreddo-f70c5b.netlify.app/#',
+        status: 'Complete',
+        img: pawesomeImage,
+        subtitle: 'Bootcamp Project #2 - Feb 2024',
+        icons: [FaCss3Alt, FaHtml5, SiJavascript, FaReact, FaBootstrap, FaNodeJs, FaNpm],
+        type: 'Web Development',
+    },
+    {
+        title: 'travel.ler',
+        description: 'A dynamic travel dashboard for visitors to the UK, providing relevant data like weather, currency and news',
+        url: 'https://lico27.github.io/travelLER/',
+        status: 'Complete',
+        img: travellerImage,
+        subtitle: 'Bootcamp Project #1 - Dec 2023',
+        icons: [FaCss3Alt, FaHtml5, SiJavascript, FaBootstrap],
+        type: 'Web Development',
     }
 ]
 
